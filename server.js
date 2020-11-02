00f077633e1d9dfd657bd22746dbad011cd30028
Evans Obeng <iamevansobeng@outlook.com>
2020-11-02 08:35:20 +0000

    Changed port and added procfile

--- server.js
+++ server.js
@@ -17,6 +17,6 @@ app.get("/", (req, res) => {
 app.use("/api/users", require("./routes/users"))
 app.use("/api/auth", require("./routes/auth"))
 app.use("/api/accounts", require("./routes/accounts"))
-const PORT = process.env.port || 5000
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
